85f70fb243c1590ab64aab8d134a7415c589fc49
Nils <nils.heesemann@rwth-aachen.de>
2021-05-29 01:32:50 +0200

    finished RKIAPI

--- coronaBot.py
+++ coronaBot.py
@@ -5,6 +5,7 @@ import pathlib
 import datetime
 import time
 import configparser
+import json
 #needs to be executed first
 runPath = pathlib.Path(__file__).parent.absolute()
 os.chdir(runPath)
@@ -14,7 +15,7 @@ config.read("config.ini")
 
 chat_id = config["botdata"]["chat_id"]
 token = config["botdata"]["token"]
-timeBetweenCurls = config["botdata"]["timeBetweenCurls"]
+timeBetweenCurls = 60#int(config["botdata"]["timeBetweenCurls"])
 url= "https://api.telegram.org/bot" + token + "/sendMessage"
 
 messageData = {
@@ -24,33 +25,29 @@ messageData = {
 lastUpdateDate = "lastupdate.txt"
 lastupdate=""
 if(not os.path.exists(lastUpdateDate)):
-    open(lastUpdateDate, "x").write("01.01.1990")
+    open(lastUpdateDate, "x").write("2021-05-20")
 with open(lastUpdateDate,"r") as f:
     lastupdate = f.readline().replace(" ","").replace("\n","")
 print(lastupdate)
 nichtDurch = True
 tries = 0
-maxTries = 24
+maxTries = 180
+
 while(nichtDurch and tries<maxTries):
-    if(datetime.datetime.strptime(lastupdate, '%d.%m.%Y').date()!=datetime.datetime.now().date() and datetime.datetime.now().hour>10):
+    if(datetime.datetime.strptime(lastupdate,'%Y-%m-%d').date()!=datetime.datetime.now().date()):
         tries+=1
-        data = requests.get('https://www.aachen.de/DE/stadt_buerger/notfall_informationen/corona/aktuelles/index.html')
-        soup = bs(data.text,'html.parser')
-        aTables = soup.findAll("table")
-        for table in aTables:
-            if(len(table.findAll("tr"))==3):
-                trs = table.findAll("tr")
-                for tr in trs:
-                    tds = tr.findAll("td")
-                    if ("RKI-Inzidenz" in tds[0].get_text()):
-                        datum = trs[1].findAll("td")
-                        datum = datum[len(datum)-1].find("div").get_text().replace("21","2021")
-                        message = "Aktuelle Inzidenz am " + datum +": "+ tds[len(tds)-1].find("div").get_text()
-                        print(message)
-                        messageData["text"]=message
-                        requests.get(url,data=messageData)
-                        open(lastUpdateDate,"w").write(datum)
-                        nichtDurch=False
+        data = requests.get("https://api.corona-zahlen.org/districts")
+        jsonObject = json.loads(data.content) 
+        datum = str(datetime.datetime.strptime(jsonObject["meta"]["lastUpdate"],'%Y-%m-%dT%H:%M:%S.%fZ').date())
+        print(datum)
+        if(str(datetime.datetime.now().date()) in datum ):#datetime.datetime.now().date()):
+            incidenceAachen = str(jsonObject["data"]["05334"]["weekIncidence"])
+            incidenceHagen = str(jsonObject["data"]["05914"]["weekIncidence"])
+            message = "Die Aktuellen Inzidenzen vom " + datum + " sind für\nAachen: " +incidenceAachen + "\nHagen: " + incidenceHagen
+            messageData["text"]=message
+            requests.get(url,data=messageData)
+            open(lastUpdateDate,"w").write(datum)
+            nichtDurch=False
         if(nichtDurch):
             time.sleep(timeBetweenCurls)
     else:
