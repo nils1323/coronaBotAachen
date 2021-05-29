459bee30a73354796bd589921c0cecf9fca5fa5d
Nils <nils.heesemann@rwth-aachen.de>
2021-05-29 02:14:47 +0200

    working Corona figures HagenAachen

--- coronaBot.py
+++ coronaBot.py
@@ -1,54 +1,39 @@
+import zoneinfo
 from bs4 import BeautifulSoup as bs
 import requests
 import os
 import pathlib
-import datetime
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 import time
 import configparser
 import json
 #needs to be executed first
 runPath = pathlib.Path(__file__).parent.absolute()
 os.chdir(runPath)
-
 config = configparser.ConfigParser()
 config.read("config.ini")
 
 chat_id = config["botdata"]["chat_id"]
 token = config["botdata"]["token"]
-timeBetweenCurls = 60#int(config["botdata"]["timeBetweenCurls"])
+
 url= "https://api.telegram.org/bot" + token + "/sendMessage"
 
-messageData = {
-    "chat_id":chat_id,
-    "text":"LOLOLOLOLO"
-}
-lastUpdateDate = "lastupdate.txt"
-lastupdate=""
-if(not os.path.exists(lastUpdateDate)):
-    open(lastUpdateDate, "x").write("2021-05-20")
-with open(lastUpdateDate,"r") as f:
-    lastupdate = f.readline().replace(" ","").replace("\n","")
-print(lastupdate)
-nichtDurch = True
-tries = 0
-maxTries = 180
+def send(message):
+    messageData = {
+        "chat_id":chat_id,
+        "text":message
+    }
 
-while(nichtDurch and tries<maxTries):
-    if(datetime.datetime.strptime(lastupdate,'%Y-%m-%d').date()!=datetime.datetime.now().date()):
-        tries+=1
-        data = requests.get("https://api.corona-zahlen.org/districts")
-        jsonObject = json.loads(data.content) 
-        datum = str(datetime.datetime.strptime(jsonObject["meta"]["lastUpdate"],'%Y-%m-%dT%H:%M:%S.%fZ').date())
-        print(datum)
-        if(str(datetime.datetime.now().date()) in datum ):#datetime.datetime.now().date()):
-            incidenceAachen = str(jsonObject["data"]["05334"]["weekIncidence"])
-            incidenceHagen = str(jsonObject["data"]["05914"]["weekIncidence"])
-            message = "Die Aktuellen Inzidenzen vom " + datum + " sind für\nAachen: " +incidenceAachen + "\nHagen: " + incidenceHagen
-            messageData["text"]=message
-            requests.get(url,data=messageData)
-            open(lastUpdateDate,"w").write(datum)
-            nichtDurch=False
-        if(nichtDurch):
-            time.sleep(timeBetweenCurls)
-    else:
-        nichtDurch=False
+    requests.get(url,data=messageData)
+
+
+data = requests.get("https://api.corona-zahlen.org/districts")
+jsonObject = json.loads(data.content)
+datum = str(datetime.strptime(jsonObject["meta"]["lastUpdate"],'%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc).astimezone(tz=None).date())
+lastUpdated = str(datetime.strptime(jsonObject["meta"]["lastCheckedForUpdate"],'%Y-%m-%dT%H:%M:%S.%fZ').replace(tzinfo=timezone.utc).astimezone(tz=None).time().replace(microsecond=0))
+print(datum)
+incidenceAachen = str(jsonObject["data"]["05334"]["weekIncidence"])
+incidenceHagen = str(jsonObject["data"]["05914"]["weekIncidence"])
+send("Inzidenzen vom " + datum + " zuletzt um " + lastUpdated + " überprüft.\nAachen: " + incidenceAachen + "\nHagen: " + incidenceHagen)
+
